2dc69584fba0c7f9f08f02bfe9a9ff73d48a5e05
Niranjan Kumar <kniranajn2903@gmail.com>
2023-04-21 00:56:10 +0530

    adding connection on mongo db

--- database/connection.js
+++ database/connection.js
@@ -3,5 +3,6 @@ import mongoose from "mongoose";
 const Connectdatabse = async () => {
     await mongoose.connect(process.env.Mongo_DB_Connection)
     console.log("connection established")
+    
 }
 export default Connectdatabse
